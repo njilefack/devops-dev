b685f419e12e13b33eb0c1680c1f1ef1c8049b40
unknown <jilefack@gmail.com>
2020-11-23 19:37:07 -0500

    modified:   backend.tf

--- modules/backend.tf
+++ modules/backend.tf
@@ -5,8 +5,8 @@ terraform {
     key    = "terraform.tfstate"
     #dynamodb_table = "statelockdb"
     region = "us-east-2"
-    access_key ="AKIAQT2EMIPJ4L27SJUX"
-    secret_key="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"
+    access_key =""
+    secret_key=""
   }
 }
 
